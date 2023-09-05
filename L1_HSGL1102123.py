386098ec9c4657f2464db1ebf492647aad263baa
Hector1102123 <143530963+Hector1102123@users.noreply.github.com>
2023-09-05 09:56:06 -0600

    Add files via upload

---
+++ L1_HSGL1102123.py
@@ -0,0 +1,29 @@
+variable1=2100
+print(variable1)
+print(type(variable1))
+
+variable1="Hola"
+print(variable1)
+print(type(variable1))
+
+variable2=12.25
+print(variable2)
+print(type(variable2))
+
+variable2=True
+print(variable2)
+print(type(variable2), end="")
+
+print("Hola mundo soy Hector)")
+
+print("Ingrese suu nombre:")
+texto= input()
+print(texto + "Hola")
+
+print("hola mundo")
+print("soy hector")
+
+#La diferecia entre print y print end es que enm print se escribe en lineas separadas, pero con print end se escribe seguido.
+
+print("Hola mundo", end="")
+print("soy Hector", end="")
