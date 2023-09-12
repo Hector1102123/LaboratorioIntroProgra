17ec2a73d77a62400c386bb21376aa25a93cb24a
Hector1102123 <143530963+Hector1102123@users.noreply.github.com>
2023-09-12 09:52:33 -0600

    Add files via upload

---
+++ L5_GLHS_1102123.py
@@ -0,0 +1,10 @@
+print("Ejercicio 1")
+v1=input("Ingrese nùmero entero")
+xnum=int(v1)
+print(type(xnum))
+if xnum>0:
+    print("Positivo")
+if xnum<0:
+    print("Negativo")
+else: 
+    print("Cero")
