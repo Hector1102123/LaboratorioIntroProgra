3690ae41843e15fdcc692a381147db701796851d
Hector1102123 <143530963+Hector1102123@users.noreply.github.com>
2023-09-26 10:12:24 -0600

    Add files via upload

---
+++ LB8_GLHS_1102123.py
@@ -0,0 +1,28 @@
+print("1. Factorial")
+print("2.Secuencia de Fibonacci")
+print("3.salir")
+print("Ingrese el valor que desea")
+x=input()
+x2=int(x)
+y=1
+yy=y+1
+numero=input("Ingrese el valor deseado:")
+numero2=int(numero)
+factorial=1
+factorial = numero2 *y2
+condicion=True
+while condicion==True:
+    if x2==1:
+        numero=input("Ingrese el valor deseado:")
+        numero2=int(numero)
+        while (y<=numero2):
+            factorial=1
+            y=1
+            factorial = factorial *y2
+            y=y+1
+            print(str(factorial))
+if x2 ==2:
+    numero3=input("Ingrese el valor deseado:")
+    numero4=int(numero3)
+
+
