2ac9d9f9f92e871807768e97f465ad4eaaa05166
Hector1102123 <143530963+Hector1102123@users.noreply.github.com>
2023-11-03 15:16:56 -0600

    Add files via upload

---
+++ Tarea 13/LB13TAREA13_GLHS_1102123.py
@@ -0,0 +1,28 @@
+#Héctor Sebastián Garrido López 1102123
+print("Semana 13")
+
+class Circulo:
+    def __init__ (self, radio):
+        self.radio=radio
+    
+    def Devolver_perimetro(self):
+        return self.radio*3.14*2
+    
+    def Devolver_Area(self):
+        return self.radio*3.14*self.radio
+
+    def Devolver_Volumen(self):
+        return (self.radio*self.radio*self.radio*4*3.14)/3
+ 
+No_circulo=int(input("Ingrese el número de circulos:"))
+i=0
+for i in range(No_circulo):
+    radio=float(input("Ingrese el valor del radio:"))
+    p1=Circulo(radio)
+    Perimetro= p1.Devolver_perimetro()
+    Area= p1.Devolver_Area()
+    Volumen= p1.Devolver_Volumen()
+    print("El perimetro es:"+str(Perimetro))
+    print("El Area es:"+str(Area))
+    print("El Volumen es:"+str(Volumen))
+
